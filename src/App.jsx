a525e800cab5b3afd1cf7bd5fa57f5abee238053
sphnikola <sphnikola@gmail.com>
2024-03-03 18:04:21 +0000

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+export default function App() {
+  return (
+    <>
+      <header className="text-white flex flex-col items-center bg-[#272829] md:max-w-6xl md:mx-auto">
+        <h1 className="">William Ofor Asante</h1>
+        <nav>
+          <ul>
+            <li>
+              <a>Linkedin</a>
+            </li>
+            <li>
+              <a>Github</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
