422f758b26da6d2b97d1708129839e45661dfa98
sphnikola <sphnikola@gmail.com>
2024-03-03 20:29:54 +0000

    Add technologies worked with

--- src/App.jsx
+++ src/App.jsx
@@ -22,60 +22,74 @@ export default function App() {
           <section className="hero">
             <div className="flex flex-col items-center text-center gap-y-10 md:items-start md:text-left lg:flex-row lg:items-center lg:justify-around">
               <div className="space-y-6">
-                <h2 className=" text-xl font-medium md:text-2xl md:pr-10">
+                <h2 className=" text-xl space-mono-regular  md:text-2xl md:pr-10 lg:max-w-sm">
                   Nice to meet you! I'm{" "}
-                  <span className="text-2xl font-bold md:text-3xl">
+                  <span className="space-mono-bold text-2xl font-bold md:text-3xl">
                     William Ofori Asante
                   </span>
                 </h2>
-                <h3 className="text-2xl font-medium md:text-3xl">
+                <h3 className="space-mono-regular text-2xl md:text-3xl">
                   I build things for the web
                 </h3>
-                <p className="px-5 text-base font-medium leading-6 md:pr-20 md:text-lg md:leading-7 md:pl-0 lg:max-w-lg">
+                <p className="px-5 text-base leading-6 md:pr-20 md:text-lg md:leading-7 md:pl-0 lg:max-w-lg space-mono-regular font-light  text-[#2f3031]">
                   I'm a front-end developer passionate about building accessible
                   web apps and quality User Interface. I always strive to make a
                   new project better than the previous one, paying attention to
                   details, well-versed in design standards and user preferences
                 </p>
                 <div>
-                  <h3>Some technologies i like to work with:</h3>
+                  <h3 className="space-mono-regular text-2xl md:text-3xl md:pr-10 lg:max-w-lg">
+                    Some technologies i like to work with:
+                  </h3>
                   <div className="flex flex-col items-center md:items-start">
-                    <div class="grid w-2/3 grid-cols-2 grid-rows-3 md:w-1/2">
+                    <div class="grid w-2/3 grid-cols-2  grid-rows-4 md:w-1/2">
                       <span className="mb-2 flex items-center">
                         <div class="mr-3  inline-block w-2 overflow-hidden">
                           <div class=" h-4  origin-top-left rotate-45 bg-[#272829]"></div>
                         </div>
-                        react
+                        HTML
                       </span>
                       <span className="mb-2 flex items-center">
                         <div class="mr-3  inline-block w-2 overflow-hidden">
                           <div class=" h-4  origin-top-left rotate-45 bg-[#272829]"></div>
                         </div>
-                        react
+                        CSS
                       </span>
                       <span className="mb-2 flex items-center">
                         <div class="mr-3  inline-block w-2 overflow-hidden">
                           <div class=" h-4  origin-top-left rotate-45 bg-[#272829]"></div>
                         </div>
-                        react
+                        JAVASCRIPT
                       </span>
                       <span className="mb-2 flex items-center">
                         <div class="mr-3  inline-block w-2 overflow-hidden">
                           <div class=" h-4  origin-top-left rotate-45 bg-[#272829]"></div>
                         </div>
-                        react
+                        REACT
                       </span>
                       <span className="mb-2 flex items-center">
                         <div class="mr-3  inline-block w-2 overflow-hidden">
                           <div class=" h-4  origin-top-left rotate-45 bg-[#272829]"></div>
                         </div>
-                        react
+                        TAILWINDCSS
                       </span>
                       <span className="mb-2 flex items-center">
                         <div class="mr-3  inline-block w-2 overflow-hidden">
                           <div class=" h-4  origin-top-left rotate-45 bg-[#272829]"></div>
                         </div>
-                        react
+                        GITHUB
+                      </span>
+                      <span className="mb-2 flex items-center">
+                        <div class="mr-3  inline-block w-2 overflow-hidden">
+                          <div class=" h-4  origin-top-left rotate-45 bg-[#272829]"></div>
+                        </div>
+                        GIT
+                      </span>
+                      <span className="mb-2 flex items-center">
+                        <div class="mr-3  inline-block w-2 overflow-hidden">
+                          <div class=" h-4  origin-top-left rotate-45 bg-[#272829]"></div>
+                        </div>
+                        ACCESSIBILITY
                       </span>
                     </div>
                   </div>
