e6fa45fa342a176f524bf68b7616b995d2fc55da
sphnikola <sphnikola@gmail.com>
2024-03-03 20:50:54 +0000

    style header section

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,10 @@ export default function App() {
     <>
       <section className=" md:max-w-6xl md:mx-auto">
         <header className="text-white px-8 py-2 flex flex-col items-center bg-[#272829] md:flex-row md:justify-between md:rounded-2xl md:mt-2 md:mx-2">
-          <h1 className="">William Ofori Asante</h1>
-          <nav>
+          <h1 className="text-3xl font-extrabold oswald-header">
+            William Ofori Asante
+          </h1>
+          <nav className="space-mono-regular">
             <ul className="flex gap-x-4">
               <li>
                 <a>Linkedin</a>
